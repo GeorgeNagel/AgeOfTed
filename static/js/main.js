0f14e9ba4d0db78a1125618576d5d9d659ee0857
George Nagel <gsnagel@gmail.com>
2015-07-12 17:18:40 -0400

    Adds wood image. needs to be transparent.

--- static/js/main.js
+++ static/js/main.js
@@ -4,11 +4,13 @@ function preload() {
     // Preload game assets
     game.load.image('grass', 'static/assets/grass.png');
     game.load.image('tree', 'static/assets/tree.png');
+    game.load.image('wood', 'static/assets/wood.png');
 }
 
 function create() {
     // Create the game tiles
     tiles = game.add.group();
+    pickups = game.add.group();
     var spritePx = 16; // Sprite pixel size
     var spriteZoom = 2;
     var treeProb = .2;
@@ -16,8 +18,8 @@ function create() {
     var rows = game.height/(spritePx * spriteZoom);
     var columns = game.width/(spritePx * spriteZoom);
 
-    var grassTiles = game.add.group();
-    var treeTiles = game.add.group();
+    grassTiles = game.add.group();
+    treeTiles = game.add.group();
 
     for (var r=0;r<rows;r++){
         for (var c=0;c<columns;c++){
@@ -27,16 +29,25 @@ function create() {
             }
             var tile = tiles.create(c*spritePx*spriteZoom, r*spritePx*spriteZoom, tileType);
             tile.age = 0;
+            tile.produced = false;
             tile.scale.setTo(spriteZoom, spriteZoom);
+            if (tileType === "grass"){
+                grassTiles.add(tile);
+            } else if (tileType === "tree"){
+                treeTiles.add(tile);
+            }
         }
     }
 }
 
 function update() {
-    for (var i=0;i<tiles.children.length;i++){
-        tiles.children[i].age ++;
-        if (tiles.children[i].age == 10){
+    for (var i=0;i<treeTiles.children.length;i++){
+        treeTiles.children[i].age ++;
+        treeTile = treeTiles.children[i];
+        if (treeTile.age >= 10 && !treeTile.produced){
             var text = game.add.text(16, 16, "junk");
+            treeTile.produced = true;
+            tiles.create(23, 23, 'grass');
         }
     }
 }
