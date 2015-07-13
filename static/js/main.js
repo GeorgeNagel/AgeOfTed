61d6e1a2bfd9ce5ad1e588fd721db922cd3a9029
George Nagel <gsnagel@gmail.com>
2015-07-13 18:03:33 -0400

    Mildly interesting mechanic.

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,19 @@
 var game = new Phaser.Game(800, 640, Phaser.AUTO, 'game-js', { preload: preload, create: create, update:update });
 
+var spritePx = 16; // Sprite pixel size
+var spriteZoom = 2;
+var treeProb = .2;
+
+var woodCount = 0;
+
+var treeDropTime = 1000;
+var pickupRotateSpeed = 1;
+
+var grassTiles = [];
+var treeTiles = [];
+
+var text;
+
 function preload() {
     // Preload game assets
     game.load.image('grass', 'static/assets/grass.png');
@@ -11,15 +25,9 @@ function create() {
     // Create the game tiles
     tiles = game.add.group();
     pickups = game.add.group();
-    var spritePx = 16; // Sprite pixel size
-    var spriteZoom = 2;
-    var treeProb = .2;
 
     var rows = game.height/(spritePx * spriteZoom);
     var columns = game.width/(spritePx * spriteZoom);
-
-    grassTiles = game.add.group();
-    treeTiles = game.add.group();
 
     for (var r=0;r<rows;r++){
         for (var c=0;c<columns;c++){
@@ -32,22 +40,44 @@ function create() {
             tile.produced = false;
             tile.scale.setTo(spriteZoom, spriteZoom);
             if (tileType === "grass"){
-                grassTiles.add(tile);
+                grassTiles.push(tile);
             } else if (tileType === "tree"){
-                treeTiles.add(tile);
+                treeTiles.push(tile);
             }
         }
     }
+    text = game.add.text(10, 10, "wood: 0", { font: "10px Arial", fill: "#000000", align: "center" });
 }
 
 function update() {
-    for (var i=0;i<treeTiles.children.length;i++){
-        treeTiles.children[i].age ++;
-        treeTile = treeTiles.children[i];
-        if (treeTile.age >= 10 && !treeTile.produced){
-            var text = game.add.text(16, 16, "junk");
+    for (var i=0;i<treeTiles.length;i++){
+        treeTiles[i].age ++;
+        treeTile = treeTiles[i];
+        if (treeTile.age >= treeDropTime && !treeTile.produced){
             treeTile.produced = true;
-            tiles.create(23, 23, 'grass');
+            wood = pickups.create(treeTile.position.x + spritePx*spriteZoom/2, treeTile.position.y + spritePx*spriteZoom/2, 'wood');
+            wood.anchor.setTo(0.5, 0.5);
+            wood.scale.setTo(spriteZoom, spriteZoom);
+            // Listen to events on this sprite
+            wood.inputEnabled = true;
+            // Send the wood in the event context
+            wood.events.onInputOver.add(pickupWood, {pickup: wood, tile: treeTile});
         }
     }
+    for (var i=0;i<pickups.children.length;i++){
+        pickups.children[i].angle += pickupRotateSpeed;
+    }
+    text.text = "Wood " + woodCount;
+}
+
+function pickupWood() {
+    woodCount += 1;
+    // Grab the pickup tile from the context
+    var wood = this.pickup;
+    // Delete the object and stop rendering it.
+    wood.destroy();
+    // Reset the age of the tile
+    var tile = this.tile;
+    tile.produced = false;
+    tile.age = 0;
 }
